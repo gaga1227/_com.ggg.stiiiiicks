e7ea638ee26104f8d0845f1c16b4781702f0d686
Johnny Xu <gaga1227@gmail.com>
2015-02-28 23:10:58 +1100

    added browsersync and serve task

--- gulpfile.js
+++ gulpfile.js
@@ -2,49 +2,70 @@
 var gulp = require('gulp');
 
 // Include Our Plugins
-var jshint = require('gulp-jshint');
 var sass = require('gulp-sass');
+var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
-// Compile Our Sass
-gulp.task('sass', function() {
+// Compile Sass & auto-inject into browsers
+gulp.task('sass', function () {
 	return gulp.src('lib/scss/**/*.scss')
 		.pipe(sass())
-		.pipe(gulp.dest('lib/css'));
+		.pipe(gulp.dest('lib/css'))
+		.pipe(reload({
+			stream: true
+		}));
 });
 
 // Lint Task
-gulp.task('lint', function() {
+gulp.task('lint', function () {
 	return gulp.src('lib/js/app/**/*.js')
 		.pipe(jshint())
 		.pipe(jshint.reporter('default'));
 });
 
-// Concatenate & Minify JS
-gulp.task('appscripts', function() {
+// Concatenate, Minify JS & auto-inject into browsers
+gulp.task('appscripts', function () {
 	return gulp.src('lib/js/app/**/*.js')
 		.pipe(concat('app.js'))
 		.pipe(gulp.dest('lib/js'))
 		.pipe(rename('app.min.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('lib/js'));
+		.pipe(gulp.dest('lib/js'))
+		.pipe(reload({
+			stream: true
+		}));
 });
 
-gulp.task('vendorscripts', function() {
+gulp.task('vendorscripts', function () {
 	return gulp.src('lib/js/vendor/**/*.js')
 		.pipe(concat('vendor.js'))
 		.pipe(gulp.dest('lib/js'))
 		.pipe(rename('vendor.min.js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('lib/js'));
+		.pipe(gulp.dest('lib/js'))
+		.pipe(reload({
+			stream: true
+		}));
 });
 
 // Watch Files For Changes
-gulp.task('watch', function() {
+gulp.task('watch', function () {
 	gulp.watch('lib/js/app/**/*.js', ['lint', 'appscripts']);
 	gulp.watch('lib/scss/**/*.scss', ['sass']);
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass', 'lint', 'appscripts'], function () {
+	browserSync({
+		server: "./"
+	});
+	gulp.watch("lib/js/app/**/*.js", ['lint', 'appscripts']);
+	gulp.watch("lib/scss/**/*.scss", ['sass']);
+	gulp.watch("./*.html").on('change', reload);
 });
 
 // Dev Tasks
@@ -54,4 +75,4 @@ gulp.task('dev', ['sass', 'lint', 'appscripts', 'watch']);
 gulp.task('build', ['sass', 'lint', 'appscripts', 'vendorscripts']);
 
 // Default Task
-gulp.task('default', ['dev']);
+gulp.task('default', ['serve']);
