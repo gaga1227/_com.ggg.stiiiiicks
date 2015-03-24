b6c7088a6d1ad9fa772df8a1689a658ceb02aac6
Johnny Xu <gaga1227@gmail.com>
2015-03-24 22:59:16 +1100

    added clean and dist tasks to build artifact for phonegap dev app

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var clean = require('gulp-clean');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
@@ -70,11 +71,30 @@ gulp.task('serve', ['sass', 'appscripts'], function () {
 	gulp.watch("./*.html").on('change', reload);
 });
 
+// clean dist folder
+gulp.task('clean', function () {
+	return gulp.src('www/**/*', {
+			read: false,
+			force: true
+		})
+		.pipe(clean());
+});
+
+// deploy app artifact
+gulp.task('dist', ['clean'], function () {
+	gulp.src('index.html').pipe(gulp.dest('www'));
+	gulp.src('lib/css/**/*.css').pipe(gulp.dest('www/lib/css'));
+	gulp.src('lib/js/*.min.js').pipe(gulp.dest('www/lib/js'));
+	gulp.src('lib/font/**/*').pipe(gulp.dest('www/lib/font'));
+	gulp.src('lib/icon/**/*').pipe(gulp.dest('www/lib/icon'));
+	gulp.src('lib/img/**/*').pipe(gulp.dest('www/lib/img'));
+});
+
 // Dev Tasks
 gulp.task('dev', ['sass', 'lint', 'appscripts', 'watch']);
 
 // Build Tasks
-gulp.task('build', ['sass', 'lint', 'appscripts', 'vendorscripts']);
+gulp.task('build', ['sass', 'lint', 'appscripts', 'vendorscripts', 'dist']);
 
 // Default Task
 gulp.task('default', ['serve']);
